7a39209d4ab1cb401b32bceb43fc9f687a058d63
YunSeok-Choi <dbstjr9898@naver.com>
2022-09-01 03:39:38 +0900

    :sparkles: 이미지 스타일 변경 기능 추가

--- BlackStyle/TransViewController.swift
+++ BlackStyle/TransViewController.swift
@@ -6,24 +6,83 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import Photos
 
-class TransViewController: UIViewController {
 
+class TransViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        
+        imagePicker.sourceType = .photoLibrary
+//        PHPhotoLibrary.shared().performChanges({
+//            PHAssetChangeRequest.creationRequestForAsset(from: self.imageView.image!)
+//        }, completionHandler: nil)
+        
+        imagePicker.sourceType = .photoLibrary
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        // 사용자가 이미지를 선택해옴
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            // 뷰를 선택된 이미지로 변경
+            self.imageView.image = userPickedImage
+            // CIImage로 변경
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Failed to CIImage")
+            }
+            // ML로 분석
+            detect(image: ciImage)
+        }
+        
+        imagePicker.dismiss(animated: true)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // 선택한 이미지를 ML을 통해 분석
+    func detect(image: CIImage) {
+        
+        // Model을 VisionModel로 변환
+        guard let model = try? VNCoreMLModel(for: Pencil(configuration: MLModelConfiguration()).model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        // Vision ML 요청값
+        let request = VNCoreMLRequest(model: model) { request, _ in
+            // 요청한 결과 값 저장
+            
+            DispatchQueue.global(qos: .userInteractive).async {
+                guard let results = request.results?.first as? VNPixelBufferObservation else {
+                    fatalError("model failed to process image")
+                }
+                let pixelBuffer = results.pixelBuffer
+                
+                let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+                
+                DispatchQueue.main.async {
+                    self.imageView.image = UIImage(ciImage: ciImage)
+                }
+            }
+            
+        }
+        //        print(request)
+        // 핸들러를 통해 이미지를 분석
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
-    */
-
+    
+    @IBAction func albumTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
